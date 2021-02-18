850f4d3b860c9b6fe284510c146139e600e2aa69
Krishaay Jois <krishaayjois@gmail.com>
2021-02-17 21:06:23 -0800

    Update main.py

--- main.py
+++ main.py
@@ -20,5 +20,5 @@ async def on_ready():
 for e in extensions:
     client.load_extension(e)
 
-client.run("Nzg0NDM3MjQ5NDY2NDMzNjE2.X8pSHg.jd1I1lPdPUxj3-D9I71PYJvpIu8")
+client.run("Nzg0NDM3MjQ5NDY2NDMzNjE2.X8pSHg.jd1I1lPdPUxj3-D9I71PYJvpIu8") # This is not the actual token this is invalid btw , dont steal ;)
     
